f29080d7bdf2f9ed29efdf64270bc6901d3b0781
Guillermo Alarcón <xxmgaxx@gmail.com>
2018-04-03 18:51:54 +0200

    feat: list and edit meal

--- controllers/meal.controller.js
+++ controllers/meal.controller.js
@@ -2,10 +2,16 @@ const mongoose = require('mongoose');
 const Meal = require('../models/meal.model');
 const ApiError = require('../models/api-error.model');
 
+module.exports.list = (req, res, next) => {
+  Meal.find()
+    .then(meal => res.json(meal))
+    .catch(error => next(error));
+}
+
 module.exports.create = (req, res, next) => {
   const meal = new Meal(req.body);
   meal.save()
-    .then(()=>{
+    .then(() => {
       res.status(201).json(meal);
     })
     .catch(error => {
@@ -19,7 +25,9 @@ module.exports.create = (req, res, next) => {
 
 module.exports.get = (req, res, next) => {
   const text = req.params.text;
-  Meal.find({name: text})
+  Meal.find({
+      name: text
+    })
     .then(meal => {
       if (meal) {
         res.json(meal)
@@ -27,4 +35,26 @@ module.exports.get = (req, res, next) => {
         next(new ApiError(`Meal not found`, 404));
       }
     }).catch(error => next(error));
+}
+
+module.exports.edit = (req, res, next) => {
+  const id = req.params.id;
+  Meal.findByIdAndUpdate(id, {
+      $set: req.body
+    }, {
+      new: true
+    })
+    .then(meal => {
+      if (meal) {
+        res.json(meal)
+      } else {
+        next(new ApiError(`Meal not found`, 404));
+      }
+    }).catch(error => {
+      if (error instanceof mongoose.Error.ValidationError) {
+        next(new ApiError(error.message, 400, error.errors));
+      } else {
+        next(new ApiError(error.message, 500));
+      }
+    });
 }
--- routes/meal.routes.js
+++ routes/meal.routes.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const mealController = require('../controllers/meal.controller');
 const secureMiddleware = require('../middleware/secure.middleware');
 
+router.get('/', mealController.list);
 router.post('/', mealController.create);
 router.get('/:text', mealController.get);
+router.put('/:id', mealController.edit);
 
 module.exports = router;
